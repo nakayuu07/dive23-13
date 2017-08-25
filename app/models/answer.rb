238d641cecd2a2f932de09af020df5260d5cf3d7
kohei <kouhik06@gmail.com>
2017-08-25 23:37:24 +0900

    set association and change routing for votes.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,5 @@ class Answer < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :question
+  has_many :votes, dependent: :destroy
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
     validates :title, presence: true
     belongs_to :user
     has_many :answers, dependent: :destroy
+    has_many :votes, dependent: :destroy
     default_scope -> { order(updated_at: :desc) }
 end
