8c5fdf50c85230e9693b06c5ddaa83b42a5db215
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-08-25 16:01:46 +0900

    make answer function

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,20 @@
 class AnswersController < ApplicationController
+
+  def create
+   @answer = current_user.answers.build(answer_params)
+   @question = @answer.question
+   respond_to do |format|
+     if @answer.save
+      format.html{ redirect_to question_path(@question), notice: 'コメントを投稿しました.' }
+      format.js { render :index }
+     else
+       format.html { render :new }
+     end
+   end
+ end
+
+ private
+   def answer_params
+     params.require(:answer).permit(:question_id, :content)
+   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
+  validates :content, presence: true
+
   belongs_to :user
   belongs_to :question
 end
