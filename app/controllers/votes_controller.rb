a7d8ecdc13b6a285c6d78a866ef20685d042e460
kohei <kouhik06@gmail.com>
2017-08-26 14:13:51 +0900

    Add before_action :authenticate_user! and strong parameter at votes_controller.rb.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,13 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
   end
 
   def destroy
   end
+
+  private
+    def vote_params
+      require(:vote).permit(:plus_or_minus, :user_id, :question_id :answer_id)
 end
