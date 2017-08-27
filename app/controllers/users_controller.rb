a3fef70f8c8943c8e72dc86b4b30f8e4155d76f3
草野 大樹 <i_am_the_voice@docomo.ne.jp>
2017-08-27 23:02:34 +0900

    マイページのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
     @answers = @user.answers.order("created_at desc")
     @favorites = @user.favorites.order("created_at desc")
     favorite_question_ids = @favorites.map { |f| f.question_id } #お気に入りに登録した質問のidを配列に格納
-    @questions_all = Question.where(id: [favorite_question_ids])
+    @favorite_questions = Question.where(id: [favorite_question_ids])
+    answers_question_ids = @answers.map { |a| a.question_id } #回答した質問のidを配列に格納
+    @answers_questions = Question.where(id: [answers_question_ids])
   end
 end
