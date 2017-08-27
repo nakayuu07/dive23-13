2806913bfd701e7004e30e5320062f277fe04762
kohei <kouhik06@gmail.com>
2017-08-27 17:09:30 +0900

    change the files for vote function

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,13 +2,20 @@ class VotesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @vote = Vote.create(user_id: current_user.id, question_id: params[:question_id])
-    @votes = Vote.where(question_id: params[:tweet_id])
+    @question = Question.find(params[:question_id])
+    vote = current_user.votes.build(question_id: @question.id)
+    unless vote.save
+      redirect_to questions_path
+    end
+    @votes = Vote.where(question_id: params[:question_id])
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, question_id: params[:quesiton_id])
-    like.destroy
-    @votes = Vote.where(tweet_id: params[:tweet_id])
+    vote = Vote.find(params[:id])
+    @question = vote.question
+    unless vote.destroy
+      redirect_to questions_path
+    end
+    @votes = Vote.where(question_id: params[:question_id])
   end
 end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,5 @@
 module VotesHelper
+  def is_vote?(question)
+    Vote.find_by(user_id: current_user.id, question_id: question.id) != nil
+  end
 end
