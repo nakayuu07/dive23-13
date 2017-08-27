5929c00668b4d1a723a2e9dc1bb59ea049ab5b52
草野 大樹 <i_am_the_voice@docomo.ne.jp>
2017-08-27 23:11:15 +0900

    変数名微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @favorites = @user.favorites.order("created_at desc")
     favorite_question_ids = @favorites.map { |f| f.question_id } #お気に入りに登録した質問のidを配列に格納
     @favorite_questions = Question.where(id: [favorite_question_ids])
-    answers_question_ids = @answers.map { |a| a.question_id } #回答した質問のidを配列に格納
-    @answers_questions = Question.where(id: [answers_question_ids])
+    answer_question_ids = @answers.map { |a| a.question_id } #回答した質問のidを配列に格納
+    @answer_questions = Question.where(id: [answer_question_ids])
   end
 end
