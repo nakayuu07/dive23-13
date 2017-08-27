079d667f163e0739cb352433f9aff544fc5bdfbc
草野 大樹 <i_am_the_voice@docomo.ne.jp>
2017-08-27 22:52:22 +0900

    userコントローラのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @questions = Question.where(user_id: params[:id]).order("created_at desc")
-    @answers = Answer.where(user_id: params[:id]).order("created_at desc")
-    @favorites = Favorite.where(user_id: params[:id]).order("created_at desc")
-    @questions_all = Question.all
+    @questions = @user.questions
+    @answers = @user.answers.order("created_at desc")
+    @favorites = @user.favorites.order("created_at desc")
+    favorite_question_ids = @favorites.map { |f| f.question_id } #お気に入りに登録した質問のidを配列に格納
+    @questions_all = Question.where(id: [favorite_question_ids])
   end
 end
