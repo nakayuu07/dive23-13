c336690a947a78818b6985be9d79a023dbe7d795
fujinojunko <fjjnmrjn@ya2.so-net.ne.jp>
2017-09-01 13:17:30 +0900

    issues/#118 votesのplus_or_minus対応

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,7 +5,23 @@ class VotesController < ApplicationController
     if params[:question_or_answer] == "question"
       @question_or_answer = "question"
       @question = Question.find(params[:question_id])
-      vote = current_user.votes.build(question_id: @question.id)
+      if params[:plus_or_minus] == "plus"
+        @plus_or_minus = "plus"
+        vote = current_user.votes.build(question_id: @question.id, plus_or_minus: 1)
+      elsif params[:plus_or_minus] == "minus"
+        @plus_or_minus = "minus"
+        vote = current_user.votes.build(question_id: @question.id, plus_or_minus: -1)
+      elsif params[:plus_or_minus] == "minustoplus"
+        @plus_or_minus = "minustoplus"
+        vote = Vote.find_by(user_id: current_user, question_id: @question.id)
+        vote.destroy
+        vote = current_user.votes.build(question_id: @question.id, plus_or_minus: 1)
+      elsif params[:plus_or_minus] == "plustominus"
+        @plus_or_minus = "plustominus"
+        vote = Vote.find_by(user_id: current_user, question_id: @question.id)
+        vote.destroy
+        vote = current_user.votes.build(question_id: @question.id, plus_or_minus: -1)
+      end
       unless vote.save
         redirect_to questions_path
       end
@@ -13,7 +29,13 @@ class VotesController < ApplicationController
       @question_or_answer = "answer"
       @answer = Answer.find(params[:answer_id])
       @question = @answer.question
-      vote = current_user.votes.build(question_id: @question.id, answer_id: @answer.id)
+      if params[:plus_or_minus] == "plus"
+        @plus_or_minus = "plus"
+        vote = current_user.votes.build(question_id: @question.id, answer_id: @answer.id, plus_or_minus: 1)
+      elsif params[:plus_or_minus] == "minus"
+        @plus_or_minus = "minus"
+        vote = current_user.votes.build(question_id: @question.id, answer_id: @answer.id, plus_or_minus: -1)
+      end
       unless vote.save
         redirect_to questions_path
       end
