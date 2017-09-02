e47829cfcdeda0c915a5ebf75647c5f908114cd6
kohei <kouhik06@gmail.com>
2017-09-02 15:26:58 +0900

    Add vote function to answer and edit vote function for question

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,12 +1,4 @@
 module VotesHelper
-  def is_vote?(question)
-    Vote.find_by(user_id: current_user.id, question_id: question.id) == nil
-  end
-
-  def is_answer_vote?(answer)
-    Vote.find_by(user_id: current_user.id, answer_id: answer.id) != nil
-  end
-
   def vote_question_plusorminus(question)
     vote = Vote.find_by(user_id: current_user.id, question_id: question.id)
     if vote.present?
@@ -14,7 +6,18 @@ module VotesHelper
     end
   end
 
+  def vote_answer_plusorminus(answer)
+    vote = Vote.find_by(user_id: current_user.id, answer_id: answer.id)
+    if vote.present?
+      vote.plus_or_minus
+    end
+  end
+
   def vote_question_sum(question)
     Vote.where(question_id: question.id).sum(:plus_or_minus)
+  end
+
+  def vote_answer_sum(answer)
+    Vote.where(answer_id: answer.id).sum(:plus_or_minus)
   end
 end
