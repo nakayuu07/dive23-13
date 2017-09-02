aea270e370978c43806eb883a7d0a2265f7cd23a
草野 大樹 <i_am_the_voice@docomo.ne.jp>
2017-09-02 23:41:52 +0900

    マイページに貢献度、投稿数情報を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,13 @@ class UsersController < ApplicationController
     @favorite_questions = Question.where(id: [favorite_question_ids])
     answer_question_ids = @answers.map { |a| a.question_id } #回答した質問のidを配列に格納
     @answer_questions = Question.where(id: [answer_question_ids])
+
+    @question_post_count = @questions.count #質問回数
+    @answer_post_count = @answers.count #回答回数
+    question_ids = @questions.map { |q| q.id } #質問のidを配列に格納
+    answer_ids = @answers.map { |a| a.id } #回答のidを配列に格納
+    question_contribution_point = Vote.where(question_id: [question_ids]).sum(:plus_or_minus) #質問で得た貢献度
+    answer_contribution_point = Vote.where(answer_id: [answer_ids]).sum(:plus_or_minus) #回答で得た貢献度
+    @contribution_point = question_contribution_point + answer_contribution_point #貢献度
   end
 end
